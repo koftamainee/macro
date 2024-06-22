19ff32b42e030a253c2df079730f45eb2b422ad6
koftamainee <koftamainee@gmail.com>
2024-06-22 17:37:33 +0300

    inital-commit

--- src/macro.c
+++ src/macro.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <termios.h>
+#include <ctype.h>
+#include <errno.h>
 
 
 typedef struct {
@@ -14,12 +16,38 @@ typedef struct {
 } PieceTable;
 
 
+void die(const char *s) {
+
+    perror(s);
+    exit(1);
+
+}
+
+
+struct termios orig_termios;
+
+void disable_raw_mode() {
+
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+        die("tcsetattr");
+
+}
+
+
 void enable_raw_mode() {
 
-    struct termios raw;
-    tcgetattr(STDIN_FILENO, &raw);
-    raw.c_lflag &= ~(ECHO);
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+    atexit(disable_raw_mode);
+
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 
 }
 
@@ -51,13 +79,22 @@ int main(int argc, char* argv[]) {
         printf("Failed to open a file.\nExit code: 2\n");
         return 2;
     }
+    
 
     /* Disable output in console */
     enable_raw_mode();
 
     /* Typo */
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    while (1) {
+    char c = '\0';
+    if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
+    if (iscntrl(c)) {
+        printf("%d\r\n", c);
+    } else {
+        printf("%d ('%c')\r\n", c, c);
+    }
+    if (c == 'q') break;
+}
 
     fclose(fptr);
     return 0;
